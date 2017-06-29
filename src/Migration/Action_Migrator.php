27b95aab61bd4028b3e3d3bb8567c44ba9bc7229
Jonathan Brinley <jbrinley@flightless.us>
2017-06-29 19:08:59 -0400

    migrate a single action from one store to another

---
+++ src/Migration/Action_Migrator.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Action_Scheduler\Custom_Tables\Migration;
+
+
+class Action_Migrator {
+	private $source;
+	private $destination;
+
+	public function __construct( \ActionScheduler_Store $source_store, \ActionScheduler_Store $destination_store ) {
+		$this->source      = $source_store;
+		$this->destination = $destination_store;
+	}
+
+	public function migrate( $source_action_id ) {
+		$action = $this->source->fetch_action( $source_action_id );
+
+		if ( ! $action->get_schedule()->next() ) {
+			// we have a null schedule (probably the action didn't exist)
+			do_action( 'action_scheduler_no_action_to_migrate', $source_action_id, $this->source, $this->destination );
+			return 0;
+		}
+
+		try {
+			$destination_action_id = $this->destination->save_action( $action );
+		} catch ( \Exception $e ) {
+			do_action( 'action_scheduler_migrate_action_failed', $source_action_id, $this->source, $this->destination );
+
+			return 0; // could not save the action in the new store
+		}
+
+		try {
+			$this->source->delete_action( $source_action_id );
+
+			do_action( 'action_scheduler_migrated_action', $source_action_id, $destination_action_id, $this->source, $this->destination );
+
+			return $destination_action_id;
+		} catch ( \Exception $e ) {
+			// could not delete from the old store
+			do_action( 'action_scheduler_migrate_action_incomplete', $source_action_id, $destination_action_id, $this->source, $this->destination );
+			do_action( 'action_scheduler_migrated_action', $source_action_id, $destination_action_id, $this->source, $this->destination );
+
+			return $destination_action_id;
+		}
+	}
+}
---
+++ tests/phpunit/migration/Action_Migrator_Test.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Action_Scheduler\Custom_Tables;
+
+use Action_Scheduler\Custom_Tables\Migration\Action_Migrator;
+use ActionScheduler_Action;
+use ActionScheduler_SimpleSchedule;
+use ActionScheduler_wpPostStore;
+
+class Action_Migrator_Test extends UnitTestCase {
+	function setUp() {
+		parent::setUp();
+		if ( ! taxonomy_exists( ActionScheduler_wpPostStore::GROUP_TAXONOMY )  ) {
+			// register the post type and taxonomy necessary for the store to work
+			$store = new ActionScheduler_wpPostStore();
+			$store->init();
+		}
+	}
+
+	public function test_migrate_from_wpPost_to_db() {
+		$source = new ActionScheduler_wpPostStore();
+		$destination = new DB_Store();
+		$migrator = new Action_Migrator( $source, $destination );
+
+		$time      = as_get_datetime_object();
+		$schedule  = new ActionScheduler_SimpleSchedule( $time );
+		$action    = new ActionScheduler_Action( 'my_hook', [], $schedule, 'my_group' );
+		$action_id = $source->save_action( $action );
+
+		$new_id = $migrator->migrate( $action_id );
+
+		// ensure we get the same record out of the new store as we stored in the old
+		$retrieved = $destination->fetch_action( $new_id );
+		$this->assertEquals( $action->get_hook(), $retrieved->get_hook() );
+		$this->assertEqualSets( $action->get_args(), $retrieved->get_args() );
+		$this->assertEquals( $action->get_schedule()->next()->format( 'U' ), $retrieved->get_schedule()->next()->format( 'U' ) );
+		$this->assertEquals( $action->get_group(), $retrieved->get_group() );
+
+		// ensure that the record in the old store does not exist
+		$old_action = $source->fetch_action( $action_id );
+		$this->assertInstanceOf( 'ActionScheduler_NullAction', $old_action );
+	}
+
+	public function test_does_not_migrate_missing_action_from_wpPost_to_db() {
+		$source = new ActionScheduler_wpPostStore();
+		$destination = new DB_Store();
+		$migrator = new Action_Migrator( $source, $destination );
+
+		$action_id = rand( 100, 100000 );
+
+		$new_id = $migrator->migrate( $action_id );
+		$this->assertEquals( 0, $new_id );
+
+		// ensure we get the same record out of the new store as we stored in the old
+		$retrieved = $destination->fetch_action( $new_id );
+		$this->assertInstanceOf( 'ActionScheduler_NullAction', $retrieved );
+	}
+}
