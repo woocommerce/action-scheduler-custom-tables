b61a2968158da956e5450f46cf1f9d8ec7b14040
Jonathan Brinley <jbrinley@flightless.us>
2017-07-28 14:40:54 -0400

    test various action statuses for migrator

--- tests/phpunit/migration/Action_Migrator_Test.php
+++ tests/phpunit/migration/Action_Migrator_Test.php
@@ -55,4 +55,76 @@ class Action_Migrator_Test extends UnitTestCase {
 		$retrieved = $destination->fetch_action( $new_id );
 		$this->assertInstanceOf( 'ActionScheduler_NullAction', $retrieved );
 	}
+
+	public function test_migrate_completed_action_from_wpPost_to_db() {
+		$source = new ActionScheduler_wpPostStore();
+		$destination = new DB_Store();
+		$migrator = new Action_Migrator( $source, $destination );
+
+		$time      = as_get_datetime_object();
+		$schedule  = new ActionScheduler_SimpleSchedule( $time );
+		$action    = new ActionScheduler_Action( 'my_hook', [], $schedule, 'my_group' );
+		$action_id = $source->save_action( $action );
+		$source->mark_complete( $action_id );
+
+		$new_id = $migrator->migrate( $action_id );
+
+		// ensure we get the same record out of the new store as we stored in the old
+		$retrieved = $destination->fetch_action( $new_id );
+		$this->assertEquals( $action->get_hook(), $retrieved->get_hook() );
+		$this->assertEqualSets( $action->get_args(), $retrieved->get_args() );
+		$this->assertEquals( $action->get_schedule()->next()->format( 'U' ), $retrieved->get_schedule()->next()->format( 'U' ) );
+		$this->assertEquals( $action->get_group(), $retrieved->get_group() );
+		$this->assertTrue( $retrieved->is_finished() );
+
+		// ensure that the record in the old store does not exist
+		$old_action = $source->fetch_action( $action_id );
+		$this->assertInstanceOf( 'ActionScheduler_NullAction', $old_action );
+	}
+
+	public function test_migrate_failed_action_from_wpPost_to_db() {
+		$source = new ActionScheduler_wpPostStore();
+		$destination = new DB_Store();
+		$migrator = new Action_Migrator( $source, $destination );
+
+		$time      = as_get_datetime_object();
+		$schedule  = new ActionScheduler_SimpleSchedule( $time );
+		$action    = new ActionScheduler_Action( 'my_hook', [], $schedule, 'my_group' );
+		$action_id = $source->save_action( $action );
+		$source->mark_failure( $action_id );
+
+		$new_id = $migrator->migrate( $action_id );
+
+		// ensure we get the same record out of the new store as we stored in the old
+		$retrieved = $destination->fetch_action( $new_id );
+		$this->assertEquals( $action->get_hook(), $retrieved->get_hook() );
+		$this->assertEqualSets( $action->get_args(), $retrieved->get_args() );
+		$this->assertEquals( $action->get_schedule()->next()->format( 'U' ), $retrieved->get_schedule()->next()->format( 'U' ) );
+		$this->assertEquals( $action->get_group(), $retrieved->get_group() );
+		$this->assertTrue( $retrieved->is_finished() );
+
+		// ensure that the record in the old store does not exist
+		$old_action = $source->fetch_action( $action_id );
+		$this->assertInstanceOf( 'ActionScheduler_NullAction', $old_action );
+	}
+
+	public function test_does_not_migrate_canceled_action_from_wpPost_to_db() {
+		$source = new ActionScheduler_wpPostStore();
+		$destination = new DB_Store();
+		$migrator = new Action_Migrator( $source, $destination );
+
+		$time      = as_get_datetime_object();
+		$schedule  = new ActionScheduler_SimpleSchedule( $time );
+		$action    = new ActionScheduler_Action( 'my_hook', [], $schedule, 'my_group' );
+		$action_id = $source->save_action( $action );
+		$source->cancel_action( $action_id );
+
+		$new_id = $migrator->migrate( $action_id );
+
+		$this->assertEquals( 0, $new_id );
+
+		// ensure that the record in the old store does not exist
+		$old_action = $source->fetch_action( $action_id );
+		$this->assertInstanceOf( 'ActionScheduler_NullAction', $old_action );
+	}
 }
