027224d7c0108cc67cba8fcb5baece6628dc2ea1
Jonathan Brinley <jbrinley@flightless.us>
2017-07-28 14:39:44 -0400

    remove function that's already been moved to the Plugin class

--- action-scheduler-custom-tables.php
+++ action-scheduler-custom-tables.php
@@ -12,14 +12,4 @@ namespace Action_Scheduler\Custom_Tables;
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
-function get_migration_config_object() {
-	$config = new Migration\Migration_Config();
-	$config->set_source_store( new \ActionScheduler_wpPostStore() );
-	$config->set_source_logger( new \ActionScheduler_wpCommentLogger() );
-	$config->set_destination_store( new DB_Store() );
-	$config->set_destination_logger( new DB_Logger() );
-
-	return apply_filters( 'action_scheduler_custom_tables_migration_config', $config );
-}
-
 add_action( 'plugins_loaded', [ Plugin::class, 'init' ], 0, 0 );
