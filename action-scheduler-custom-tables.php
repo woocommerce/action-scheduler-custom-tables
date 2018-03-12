6d0c37ffb2f6127631a27f854219316be8ce8c3c
Jeremy Pry <jeremy.pry@gmail.com>
2018-03-12 12:45:50 -0400

    Update format of main file docblock

--- action-scheduler-custom-tables.php
+++ action-scheduler-custom-tables.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: Action Scheduler - Custom Tables
-Plugin URI: https://github.com/prospress/action-scheduler-custom-tables
-Description: Improved scalability for the Action Scheduler library using custom tables
-Author: Prospress
-Author URI: http://prospress.com/
-Version: 1.0.0-dev
-*/
+/**
+ * Plugin Name: Action Scheduler - Custom Tables
+ * Plugin URI: https://github.com/prospress/action-scheduler-custom-tables
+ * Description: Improved scalability for the Action Scheduler library using custom tables
+ * Author: Prospress
+ * Author URI: http://prospress.com/
+ * Version: 1.0.0-dev
+ */
 
 namespace Action_Scheduler\Custom_Tables;
 
