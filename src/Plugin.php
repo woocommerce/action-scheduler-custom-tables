7e5422b234c860c14a85f164e4d21c4403e5af31
Ron Rennick <ron@ronandandrea.com>
2019-03-01 12:04:18 -0400

    bump max version for testing, fix dependency check

--- src/Plugin.php
+++ src/Plugin.php
@@ -140,10 +140,9 @@ class Plugin {
 	}
 
 	public static function init() {
-
 		self::instance();
 
-		if ( ! self::$dependency_handler->dependencies_met() ) {
+		if ( self::$dependency_handler->dependencies_met() ) {
 			self::instance()->hook();
 		} else {
 			self::$dependency_handler->add_notice();
